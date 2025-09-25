944308ac9029d1046816ae32d6f49bcf7d7b94e5
Hansa Sandamal <48385589+HansaSandamal@users.noreply.github.com>
2025-09-25 21:18:51 +0530

    refactor: Remove redundant icon assets from manifest and SW cache
    Removes PNG icon assets from the web app manifest and the service worker's cache configuration. The SVG icon is sufficient for all resolutions and simplifies asset management.

--- public/sw.js
+++ public/sw.js
@@ -3,9 +3,7 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/assets/icon.svg',
-  '/assets/icon-192x192.png',
-  '/assets/icon-512x512.png'
+  '/assets/icon.svg'
 ];
 
 // Install event: cache all core assets
