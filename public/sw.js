50d93103dc1d441283eacaebda112d1c4e8919e9
Hansa Sandamal <48385589+HansaSandamal@users.noreply.github.com>
2025-09-25 21:26:58 +0530

    feat: Add PWA icon assets and caching
    Include PNG icon variants for better PWA experience and update the service worker to cache these new assets.

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,9 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/assets/icon.svg'
+  '/assets/icon.svg',
+  '/assets/icon-192x192.png',
+  '/assets/icon-512x512.png'
 ];
 
 // Install event: cache all core assets
